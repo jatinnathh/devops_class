e4b17c55745ba2b82e093fb4bc8ce94142acbcfc
jatinnathh <jatinnath1111@gmail.com>
2026-03-24 12:45:41 +0530

    added CORS fix

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, jsonify
+from flask_cors import CORS   # 👈 ADD THIS
 
 app = Flask(__name__)
+CORS(app)  
 
 @app.route('/student-details', methods=['GET'])
 def student_details():
